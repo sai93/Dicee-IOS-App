58ae3b5e27707a85f154718732bb619ab5cf385d
iamspeed <iamspeed32@gmail.com>
2019-06-18 01:03:29 -0700

    Initial Template code files

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by iamspeed on 6/17/19.
+//  Copyright © 2019 Sai Manideep J. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
