ac254276f4f319b7e2586759bdd0050735a1e74c
iamspeed <iamspeed32@gmail.com>
2019-06-18 20:31:21 -0700

    Added randomization at the app start and optimized the code

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -23,19 +23,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateDiceImages()
     }
     
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         // This will get executed when roll button is pressed
+        updateDiceImages()
+        
+    }
+    
+    func updateDiceImages(){
         randomDiceIndex1 = Int.random(in: 0 ... 5)
         randomDiceIndex2 = Int.random(in: 0 ... 5)
-        //print(randomDiceIndex1)
         
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
-        
     }
     
 
